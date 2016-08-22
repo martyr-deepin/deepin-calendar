7617c9fac302e622eddbabea3fb74392c89f2fc1
Hualet Wang <mr.asianwang@gmail.com>
2016-08-22 14:17:34 +0800

    Update translations
    Change-Id: Id3393d7cb700ac632510c1b5f9c6f2d8342f6f01

--- translations/dde-calendar.ts
+++ translations/dde-calendar.ts
@@ -4,33 +4,33 @@
 <context>
     <name>CalendarWindow</name>
     <message>
-        <location filename="../calendarwindow.cpp" line="182"/>
+        <location filename="../calendarwindow.cpp" line="183"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="183"/>
+        <location filename="../calendarwindow.cpp" line="184"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../calendarwindow.cpp" line="30"/>
-        <location filename="../calendarwindow.cpp" line="187"/>
+        <location filename="../calendarwindow.cpp" line="188"/>
         <source>Calendar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="190"/>
+        <location filename="../calendarwindow.cpp" line="191"/>
         <source>Deepin Calendar</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="191"/>
+        <location filename="../calendarwindow.cpp" line="192"/>
         <source>Version: %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="192"/>
+        <location filename="../calendarwindow.cpp" line="193"/>
         <source>Calendar is a date tool.</source>
         <translation type="unfinished"></translation>
     </message>
--- translations/dde-calendar_zh_CN.ts
+++ translations/dde-calendar_zh_CN.ts
@@ -4,35 +4,43 @@
 <context>
     <name>CalendarWindow</name>
     <message>
-        <location filename="../calendarwindow.cpp" line="141"/>
+        <location filename="../calendarwindow.cpp" line="183"/>
         <source>About</source>
         <translation>关于</translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="142"/>
+        <location filename="../calendarwindow.cpp" line="184"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="18"/>
-        <location filename="../calendarwindow.cpp" line="146"/>
+        <location filename="../calendarwindow.cpp" line="30"/>
+        <location filename="../calendarwindow.cpp" line="188"/>
         <source>Calendar</source>
         <translation>日历</translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="149"/>
+        <location filename="../calendarwindow.cpp" line="191"/>
         <source>Deepin Calendar</source>
         <translation>深度日历</translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="150"/>
+        <location filename="../calendarwindow.cpp" line="192"/>
         <source>Version: %1</source>
         <translation>版本：%1</translation>
     </message>
     <message>
-        <location filename="../calendarwindow.cpp" line="151"/>
+        <location filename="../calendarwindow.cpp" line="193"/>
         <source>Calendar is a date tool.</source>
         <translation>日历是一款查看日期的小工具。</translation>
+    </message>
+</context>
+<context>
+    <name>InfoView</name>
+    <message>
+        <location filename="../infoview.cpp" line="25"/>
+        <source>Today</source>
+        <translation>返回今天</translation>
     </message>
 </context>
 </TS>
