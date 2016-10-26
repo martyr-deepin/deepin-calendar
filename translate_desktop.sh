e84f36f55526cd7b4c24c11a1e6ec963b6fc5284
Hualet Wang <mr.asianwang@gmail.com>
2016-10-26 15:52:40 +0800

    Fix source file & temp file misplaced
    Change-Id: Ie43cb4401cf7087947e705a516305bd41811f234

--- translate_desktop.sh
+++ translate_desktop.sh
@@ -5,5 +5,5 @@ DESKTOP_SOURCE_FILE=deepin-calendar.desktop
 DESKTOP_DEST_FILE=deepin-calendar.desktop
 DESKTOP_TS_DIR=translations/desktop/
 
-/usr/bin/deepin-desktop-ts-convert ts2desktop $DESKTOP_TEMP_FILE $DESKTOP_TS_DIR $DESKTOP_SOURCE_FILE
+/usr/bin/deepin-desktop-ts-convert ts2desktop $DESKTOP_SOURCE_FILE $DESKTOP_TS_DIR $DESKTOP_TEMP_FILE
 mv $DESKTOP_TEMP_FILE $DESKTOP_DEST_FILE
