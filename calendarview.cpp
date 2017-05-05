c351e636443738e54543698c270958c8ff8d561d
kirigaya <kirigaya@mkacg.com>
2017-05-05 11:23:56 +0800

    Fix date is not accurate
    Change-Id: Ie1b1e9f3f975dfdd7805b03b1c6e36a215b01a2e

--- calendarview.cpp
+++ calendarview.cpp
@@ -220,18 +220,14 @@ bool CalendarView::eventFilter(QObject *o, QEvent *e)
 void CalendarView::updateDate()
 {
     const QDate firstDay(m_currentDate.year(), m_currentDate.month(), 1);
-    const int daysOfCal = (firstDay.dayOfWeek() % 7) + m_currentDate.day() - 1;
-
-    const int y = (m_currentDate.dayOfWeek() + m_firstWeekDay) % 7;
-    const int x = daysOfCal / 7;
-    const int currentIndex = x * 7 + y;
+    const int day = (firstDay.dayOfWeek() + m_firstWeekDay) % 7;
+    const int currentIndex = day + m_currentDate.day() - 1;
 
     for (int i(0); i != 42; ++i) {
-        m_days[i] = m_currentDate.addDays(i - currentIndex);
+        m_days[i] = firstDay.addDays(i - day);
     }
 
     setSelectedCell(currentIndex);
-
     update();
 }
 
